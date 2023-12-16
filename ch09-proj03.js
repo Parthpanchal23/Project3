2d9ba00d69ac0e3546eaefe486adb8a82c8bd100
Parth Panchal <parthpanchal.2023@gmail.com>
2023-12-16 09:39:18 -0500

    commit 4

--- ch09-proj03.js
+++ ch09-proj03.js
@@ -5,5 +5,19 @@ document.addEventListener('DOMContentLoaded',()=>{
     const userData = JSON.parse(userContent);
     const stocks =JSON.parse(stockContent);
     document.getElementsByClassName('Details')[0].computedStyleMap.display ="none";
-    
+    const UserList = document.getElementsByClassName("UserList")[0].children['1'];
+
+    for (let i = 0; i < userData.length; i++) {
+        
+        const LastName = userData[i]['user']['lastname'];
+        const FirstName = userData[i]['user']['firstname'];
+        const UserId = userData[i]['id'];
+
+        let LiElement = document.createElement('li');
+        LiElement.dataset.uid = UserId;
+        LiElement.myParam = userData;
+        LiElement.textContent =`${FirstName,LastName}`;
+
+        UserList.append(LiElement);
+    }
 })
