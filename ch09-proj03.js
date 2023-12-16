e2a345b77e8e05a8dca9fd8ee9e1d51f40de61e0
Parth Panchal <parthpanchal.2023@gmail.com>
2023-12-16 09:15:55 -0500

    comit 3

--- ch09-proj03.js
+++ ch09-proj03.js
@@ -1,2 +1,9 @@
 /* add your code here */
 
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const userData = JSON.parse(userContent);
+    const stocks =JSON.parse(stockContent);
+    document.getElementsByClassName('Details')[0].computedStyleMap.display ="none";
+    
+})
